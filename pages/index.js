d6a42607bcf47dd2eb098a55791053494cbc34e4
Sergey Kostik <79771862+serg-by@users.noreply.github.com>
2022-07-29 16:36:21 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
   });
 
   const handleSubmit = async values => {
-    const request = await fetch('/api/submit', {
+    const request = await fetch('/api/teleg', {
       method: 'POST',
       body: JSON.stringify(values)
     });
